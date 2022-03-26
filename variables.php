6e2d98bcfd4f3703e8cbfb28caeaf8eae8adb39a
Tiberiu Ion <tiberiu.vi@gmail.com>
2022-03-26 16:12:23 +0000

    update variables and add notes

--- variables.php
+++ variables.php
@@ -9,12 +9,21 @@
 <body>
     <h1>Variable basics</h1>
     <?php
+
+    // variables get declared with a dollar sign $
+    // a variable name must start with a letter or underscore
+    // a variable name cannot start with a number
+    // a variable name can only contain alphanumeric characters and underscores
+    // variable names are case sensitive
     $descriptiveVariableName = "value";
-    $characterName = "George";
-    $character = 70;
+    $characterName = "Tom";
+    $character = 29;
 
         echo "There was once a man named $characterName </br>";
         echo "He was $character years old. </br>";
+    
+    $characterName = "Jerry";
+    // You can update a variable midway through the program.
         echo "He really liked the name $characterName </br>";
         echo "But did like being $character </br>";
     ?>
