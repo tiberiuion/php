26b2256ccdb6379a89a67e295c123d7ffa62b38b
Tiberiu Ion <tiberiu.vi@gmail.com>
2022-03-26 15:23:37 +0000

    initial commit

---
+++ site.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP tutorial</title>
+</head>
+<body>
+    <h1>PHP tutorial</h1>
+    <?php
+        echo"<h2>This is an h2</h2>";
+        echo "<hr>";
+        echo "<p>This is my site</p>";
+    ?>
+</body>
+</html>
---
+++ variables.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+</head>
+<body>
+    <h1>Variable basics</h1>
+    <?php
+        echo"There was once a man named George </br>";
+        echo "He was 70 years old. </br>";
+        echo "He really liked the name George </br>";
+        echo "But did like being 79 </br>";
+    ?>
+</body>
+</html>
