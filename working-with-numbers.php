dc0e5ce6682eaad7a4eedc9dff8959880196f8a6
Tiberiu Ion <tiberiu.vi@gmail.com>
2022-03-27 11:33:50 +0100

    basic math and number operations

--- working-with-numbers.php
+++ working-with-numbers.php
@@ -9,7 +9,60 @@
 <body>
     <h1>Working with numbers</h1>
     <?php
-    //code goes here
+    // print out a numebr positive or negative
+    echo 40;
+    echo "<hr>";
+    echo -40;
+    echo "<hr>";
+
+    // print decimal (floaiting point numbers)
+    echo 40.47;
+    echo "<hr>";
+
+    // you can do arithmetic: addition +, subtration -, multiplication *, division /
+    echo 5.3 + 9;
+    echo "<hr>";
+
+    // modulus operator - it divides the first number by the second one and returns the remaider
+    // you read the following as "10 mod 3"
+    echo 10 % 3;
+    echo "<hr>";
+    // order of operations - same as the normal math order of operation
+    echo 4 + 5 * 10; // multiplication gets resolved first: 4 + 50 = 54
+    echo "<hr>";
+    echo ( 4 + 5 ) * 10; // parenthesis gets resolved first then the multiplication: 9 *  10 = 90
+    echo "<hr>";
+    // store a number in a variable
+    $num = 10;
+    echo $num; // 10
+    echo "<hr>";
+    // increment a number in a variable
+    $num++;
+    echo $num; // 11
+    echo "<hr>";
+    // decrement a number in a variable
+    $num--;
+    echo $num; // 10
+    echo "<hr>";
+
+    // add a number on to the variable
+    $num = $num + 25; // 35
+    echo $num;
+    echo "<hr>";
+    // shorthand version of the above
+    $num += 25; // 60
+    echo $num;
+    echo "<hr>";
+    // same shorthand works for subtraction, division, multiplication etc.
+    $num -= 35; // 25
+    echo $num;
+    echo "<hr>";
+    $num *= 2; // 50
+    echo $num;
+    echo "<hr>";
+    $num /= 2; // 25
+    echo $num;
+    echo "<hr>";
     ?>
 </body>
 </html>
