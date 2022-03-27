8843a86e153c15f356c81692dcd35bfb3c0f1bfa
Tiberiu Ion <tiberiu.vi@gmail.com>
2022-03-27 17:30:32 +0100

    build basic calculator and add notes for ruther research

--- further-research.md
+++ further-research.md
@@ -4,3 +4,6 @@ Document concepts and stuff I need to look up after the course.
 ## Workign with numbers
 1. abs() function;
 1. go online and search "php math functions"
+
+## project-1-basic-calculator
+What is $_GET["x"] and where does it come from?
--- project-1-basic-calculator.php
+++ project-1-basic-calculator.php
@@ -8,12 +8,18 @@
 </head>
 <body>
     <h1>Basic calculator</h1>
+    <!-- set up from with action attribute set to namge of file and methos set to GET -->
     <form action="project-1-basic-calculator.php" method="GET">
-
+        <!-- set up input fields -->
+        <input type="number" name="num1">
+        <input type="number" name="num2">
         <input type="submit">
     </form>
     <?php
-    //code goes here
+    // echo the sum of the two numbers
+    Answer: echo $_GET["num1"] + $_GET["num2"];
     ?>
+    <!-- note: when submitting the input values get displayed in the address bar.
+    Changing these alters the result. -->
 </body>
 </html>
