6de5aea7ae20879fd6fd0dcafb9ccce1f3041699
Tiberiu Ion <tiberiu.vi@gmail.com>
2022-04-02 18:49:48 +0100

    add simple input

--- url-parameters.php
+++ url-parameters.php
@@ -8,8 +8,15 @@
 </head>
 <body>
     <h1>URL parameters</h1>
+    <form action="url-parameters.php" action="get">
+        Name: <input type="text" name="name"> <br>
+        <input type="submit">
+    </form>
     <?php
-    // code goes here
+        // prints the contents of the name field, nothing fancy
+        // also the input filed content gets added to the URL as a parameter i.e. domain.name?url=parameter
+        //the question mark is the delimited between the path and the URL parameters 
+        echo $_GET['name'];
     ?>
 </body>
 </html>
