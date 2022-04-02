f7139c9d44ac91f7effd69420fab2ea8c8102fb0
Tiberiu Ion <tiberiu.vi@gmail.com>
2022-04-02 14:28:17 +0100

    set up form

--- project-2-mad-libs-game.php
+++ project-2-mad-libs-game.php
@@ -8,10 +8,16 @@
 </head>
 <body>
     <h1>project 2 - Mad libs game</h1>
+    <form action="project-2-mad-libs-game.php" method="GET">
+        Color: <input type="text" name="colour"> </br>
+        Plural Noun: <input type="text" name="pluralNoun"> </br>
+        Celebrity: <input type="text" name="celebrity">  </br>
+        <input type="submit">
+    </form>
     <?php
-    echo "Roses are red <br>";
-    echo "Violets are blue <br>";
-    echo "I love you <br>";
+    echo "Roses are {colour} <br>";
+    echo "{noun plural} are blue <br>";
+    echo "I love {celebrity} <br>";
     ?>
 </body>
 </html>
