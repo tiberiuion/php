d98e4ec4a95ccd08e8286b4495951864d39d13ec
Tiberiu Ion <tiberiu.vi@gmail.com>
2022-04-02 19:03:29 +0100

    set up simple form

--- get-vs-post.php
+++ get-vs-post.php
@@ -8,8 +8,15 @@
 </head>
 <body>
     <h1>Get vs post</h1>
+    <form action="get-vs-post.php" method="get">
+        Password: <input type="password" name="password">
+        <input type="submit">
+    </form>
     <?php
-        // code goes here
+        // not only does this prints the contents of the password input
+        // it also attaches that data as a URL parameter which is no bueno for passowrds
+        // so in order to get info in a secure way we need to use POST in the method attribute above.
+        echo $_GET["password"]
     ?>
 </body>
 </html>
