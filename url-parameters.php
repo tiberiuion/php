f1c8e484d9bc83e4a3472d54619e1183d95a0bae
Tiberiu Ion <tiberiu.vi@gmail.com>
2022-04-02 18:53:11 +0100

    add comments

--- url-parameters.php
+++ url-parameters.php
@@ -15,8 +15,11 @@
     <?php
         // prints the contents of the name field, nothing fancy
         // also the input filed content gets added to the URL as a parameter i.e. domain.name?url=parameter
-        //the question mark is the delimited between the path and the URL parameters 
-        echo $_GET['name'];
+        //  the question mark is the delimited between the path and the URL parameters 
+        // ---
+        // URL params can be manually added to manipulate the PHP program.
+        // add a param ?age=10 to the URL and press enter
+        echo $_GET['age']; // prints whatever the age URL param was set to, in my case, 10
     ?>
 </body>
 </html>
